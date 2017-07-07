4c44711ae1bbbe7628255b30cf981b502fb5bdd4
chentairan <2856743386@qq.com>
2017-07-07 22:06:32 +0800

    更新颜色，更加协调，加入滚动功能

--- 微信小程序项目/app.js
+++ 微信小程序项目/app.js
@@ -1,12 +1,15 @@
 //app.js
+var qcloud = require('./bower_components/qcloud-weapp-client-sdk/index.js');
+
 App({
+  
   onLaunch: function() {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
-
+ 
   getUserInfo: function(cb) {
     var that = this
     if (this.globalData.userInfo) {
