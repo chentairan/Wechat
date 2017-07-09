86240d6d105336f4779a9480dd997e6df2240e0a
collapsar <839046168@qq.com>
2017-07-09 17:02:42 +0800

    123

---
+++ 微信小程序项目/pages/logs/index.js
@@ -0,0 +1,50 @@
+Page({
+  data: {
+    message:'12345',
+    test:'54321'
+  },
+  onLoad: function (options) {
+    // Do some initialize when page load.
+  },
+  onReady: function () {
+    // Do something when page ready.
+  },
+  onShow: function () {
+    // Do something when page show.
+  },
+  onHide: function () {
+    // Do something when page hide.
+  },
+  onUnload: function () {
+    // Do something when page close.
+  },
+  onPullDownRefresh: function () {
+    // Do something when pull down.
+  },
+  onReachBottom: function () {
+    // Do something when page reach bottom.
+  },
+  onShareAppMessage: function () {
+    // return custom share data when user share.
+  },
+  // Event handler.
+  viewTap: function () {
+    this.setData({
+      text: 'Set some data for updating view.'
+    })
+  },
+  customData: {
+    hi: 'MINA'
+  },
+test1: function(){
+  this.setData({
+    message: 'test xzw'
+  })
+},
+test2:function(){
+  this.setData({
+    test: 'xzw tests'
+  })
+}
+
+})
