fcaaf3768f89ebb268a9a960c4ffb05efeebb205
collapsar <839046168@qq.com>
2017-07-09 17:08:58 +0800

    1234

--- 微信小程序项目/pages/first/first.js
+++ 微信小程序项目/pages/first/first.js
@@ -1,4 +1,4 @@
-// pages/first/first.js
+// first.js
 Page({
 
   /**
--- 微信小程序项目/pages/second/second.js
+++ 微信小程序项目/pages/second/second.js
@@ -1,45 +1,18 @@
 // second.js
-var app=getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: 
-  {
-    userInfo: {},
-    showDialog: false,
-    list:
-    [
-      {
-        name:'意见反馈',
-        path:'/pages/third/third'
-      },
-      {
-        name:'关于我们',
-        path:'/pages/fourth/fourth'
-      }
-    ]
+  data: {
+  
   },
-  toggleDialog() {
-    this.setData
-    ({
-      showDialog: !this.data.showDialog
-    });
-    },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) 
-  {
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
-    //更新数据
-    that.setData({
-    userInfo: userInfo
-    })
-  })
+  onLoad: function (options) {
+  
   },
 
   /**
@@ -74,14 +47,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    wx.showNavigationBarLoading() //在标题栏中显示加载
-
-    //模拟加载
-    setTimeout(function () {
-      // complete
-      wx.hideNavigationBarLoading() //完成停止加载
-      wx.stopPullDownRefresh() //停止下拉刷新
-    }, 1500);
+  
   },
 
   /**
