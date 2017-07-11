2c2258fde9f19b8f352febdfbc1290028275fdf6
chentairan <2856743386@qq.com>
2017-07-11 13:42:18 +0800

    整合代码，加入后端测试代码

--- 微信小程序项目/pages/first/first.js
+++ 微信小程序项目/pages/first/first.js
@@ -1,69 +1,58 @@
 // first.js
+var Bmob = require('../../utils/bmob.js');
+var common = require('../../utils/common.js');
+var building=1;
+var floor=3;
+var classroom=8;
+var time=0;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list:[
+    /*初始化数据*/
+  
+    list: [
       {
-        id:"one",
-        name:"教学楼",
-        open:false,
+        id: "one",
+        name: "教学楼",
+        open: false,
         list_2: ["理教", "综教A", "综教B"]
       },
       {
-        id:"two",
-        name:"楼层",
-        open:false,
+        id: "two",
+        name: "楼层",
+        open: false,
         list_2: ["一层", "二层", "三层", "四层", "五层"]
       },
       {
-        id:"three",
-        name:"教室",
-        open:false,
+        id: "three",
+        name: "教室",
+        open: false,
         list_2: ["普通教室", "多媒体教室"]
       },
       {
-        id:"four",
-        name:"时间",
-        open:false,
-        list_2:["1","2","3","4","5","6","7","8","9"]
+        id: "four",
+        name: "时间",
+        open: false,
+        list_2: ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       }
     ]
   },
 
-  tap: function (e) {
-    var id = e.currentTarget.id, list = this.data.list;
-    for (var i = 0, len = list.length; i < len; ++i) {
-      if (list[i].id == id) {
-        list[i].open = !list[i].open;
-      } else {
-        list[i].open = false;
+  Search:function()
+  {
+    Bmob.Cloud.run('Search', { "building":{building}, "classroom":{classroom}, "floor":{floor} }, {
+      success: function (result) {
+        var test=result;
+        console.log(test);
+      },
+      error: function (error) {
       }
-    }
-    this.setData({
-      list: list
-    });
+    })
   },
 
-  /*
-  buttonli:function() 
-  {
-    console.log('点击理教')
-    this.setData({text:true})
-  },
-  buttonza:function()
-  {
-    console.log('点击综教 A')
-    this.setData({image:true})
-  },
-  buttonzb:function()
-  {
-    console.log('点击综教 B')
-    this.setData({image:false,text:false})
-  },
-  */
   /**
    * 生命周期函数--监听页面加载
    */
@@ -102,15 +91,9 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-    //下拉更新
-   onPullDownRefresh: function() {
-      if (app._user.is_bind) {
-        this.getCardData();
-      } else {
-        wx.stopPullDownRefresh();
-      }
+  onPullDownRefresh: function () {
+  
   },
-
 
   /**
    * 页面上拉触底事件的处理函数
@@ -123,9 +106,6 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    return {
-      title: 'BIT教室',
-      path: '/page/first'
-    }
+  
   }
 })
