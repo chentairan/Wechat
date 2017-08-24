b19b9c314dea7083d00ce012c4b6543d2c5de825
chentairan <2856743386@qq.com>
2017-08-24 18:55:09 +0800

    完成Matlab程序

--- Process.m
+++ Process.m
@@ -1,7 +1,14 @@
-function [ A ] = Process()
+function [ Final_Data ] = Process()
 % Process 数据处理函数
 %   处理教室信息生成矩阵
-
-
+Final_Data=[];
+%导入初始数据
+Init_Data=xlsread('空教室信息.xlsx');
+for i=1:size(Init_Data,1)
+    for j=Init_Data(i,4):Init_Data(i,5)
+        temp=[Init_Data(i,1:3),j,Init_Data(i,6:8)];
+        Final_Data=[Final_Data temp'];
+    end
 end
-
+Final_Data=Final_Data';
+end
