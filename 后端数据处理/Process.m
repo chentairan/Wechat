43aba76cd83d7e6c68dc38797bfa4b1b6523d411
chentairan <2856743386@qq.com>
2017-08-25 15:52:52 +0800

    更新Matlab程序

--- 后端数据处理/Process.m
+++ 后端数据处理/Process.m
@@ -1,11 +1,13 @@
 function [ Final_Data ] = Process()
 % Process 数据处理函数
 %   处理教室信息生成矩阵
+
 Final_Data=[];
 
 %导入初始数据
+
 Init_Data=xlsread('空教室信息.xlsx');
-[Conv_Datan,Conv_Datae]=xlsread('教室信息.xlsx');
+Conv_Data=xlsread('教室信息.xlsx');
 
 %展开周数
 
@@ -16,15 +18,13 @@ for i=1:size(Init_Data,1)
     end
 end
 Final_Data=Final_Data';
-%Final_Data=mat2cell(Final_Data,[size(Final_Data,1)],[1,1,1,1,1,1]);
+
 %添加教室类型和规模
 
 for i=1:size(Final_Data,1)
-    if Final_Data(i,3)<9
-        class_num=['0',num2str(Final_Data(i,3))];
-    end
-    class={[num2str(Final_Data(i,1)),num2str(Final_Data(i,2)),class_num]};
-    
+    j=find(Final_Data(i,1)==Conv_Data(:,1)&Final_Data(i,2)==Conv_Data(:,2)&Final_Data(i,3)==Conv_Data(:,3));
+    Final_Data(i,7)=Conv_Data(j,4);
+    Final_Data(i,8)=Conv_Data(j,5);
 end
 
 
